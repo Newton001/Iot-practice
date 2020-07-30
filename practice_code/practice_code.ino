7d134dd89e39219088ffeee6a9326c76265ac136
Newton001 <45996137+Newton001@users.noreply.github.com>
2020-07-30 08:49:37 +0300

    Create practice_code.ino

---
+++ practice_code/practice_code.ino
@@ -0,0 +1,14 @@
+int led = 1;
+
+void setup() {
+  // put your setup code here, to run once:
+pinMode(led,OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+digitalWrite(led, HIGH);
+delay(2000);
+digitalWrite(led, LOW);
+delay(2000);
+}
